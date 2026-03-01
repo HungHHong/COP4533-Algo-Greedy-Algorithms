fb4d4167a65fd68b239cecea4828f1cf1231331f
HungHong <176441814+HungHHong@users.noreply.github.com>
2026-02-28 21:47:33 -0500

    refactor: File structure setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// Author: Hong
+
+
+#include <vector>
+using namespace std;
+
+int fifo(int k, const vector<int>& req);
+int lru(int k, const vector<int>& req);
+int optff(int k, const vector<int>& req);
